8842c968592104d35dd04c4d0696386be2876468
vkotu <vkotu@paypal.com>
2023-04-02 10:59:07 -0700

    option1

--- js/dialer.js
+++ js/dialer.js
@@ -4,25 +4,64 @@ export default {
 	listAcyclicPaths
 };
 
+var dialPad = [
+	[1,2,3],
+	[4,5,6],
+	[7,8,9],
+	[ ,0, ]
+]
 
+var nearByKeys = [
+	[4,6],
+	[6,8],
+	[7,9],
+	[4,8],
+	[3,9,0],
+	[],
+	[1,7,0],
+	[2,6],
+	[1,3],
+	[2,4]
+]
 // ****************************
 
 function reachableKeys(startingDigit) {
-	// TODO: return which digits a Knight's move
-	// can hop to from a given starting digit/key
-	//
-	// e.g. 3 -> [ 4, 8 ]
-	//      4 -> [ 3, 9, 0 ]
-	//      5 -> []
-	return [];
+
+	return nearByKeys[startingDigit];
+	// var nearByKeys = [];
+	// for(let [rowIdx, row] of dialPad.entries()) {
+	// 	let colIdx = row.indexOf(startingDigit);
+	// 	if(colIdx != -1) {
+	// 		for(let rowMove of [-2,-1,1,2]) {
+	// 			for(let colMove of [-2,-1,1,2]){
+	// 				if(Math.abs(rowMove) != Math.abs(colMove)){
+	// 					if(
+	// 						rowIdx+rowMove >=0 &&
+	// 						rowIdx+rowMove <=3 &&
+	// 						colIdx+colMove <=2 &&
+	// 						dialPad[rowIdx+rowMove][colIdx+colMove] != undefined
+	// 					) {
+	// 						nearByKeys.push(
+	// 							dialPad[rowIdx+rowMove][colIdx+colMove]
+	// 						);
+	// 					}
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// }
+	// return nearByKeys;
 }
 
 function countPaths(startingDigit,hopCount) {
-	// TODO: given the digit/key to start from and
-	// the number of hops to take, return a count
-	// of all the possible paths that could be
-	// traversed
-	return 0;
+	if(hopCount == 0) {
+		return 1;
+	}
+	var pathCount = 0;
+	for(let digit of nearByKeys[startingDigit]) {
+		pathCount += countPaths(digit, hopCount-1);
+	}
+	return pathCount;
 }
 
 function listAcyclicPaths(startingDigit) {
